74e0a32ff307adadba374c2fce91e8368272d461
sanjaysan2023-dot <sanjaysan2023@gmail.com>
2025-08-16 22:36:19 +0530

    Add files via upload
    Day1_Assignment1 
    Create a Class named Firefox with variables of given  datatypes with values (int, String,  float, boolean, char)

---
+++ Assignment1/Firefox.java
@@ -0,0 +1,20 @@
+package week1.day1.Assignment1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		
+		double browserVersion=100.2;
+		String browserName="firefox";
+		boolean isVisible=true;
+		int releaseYear=1998;
+		char browserLogo='f';
+		
+		
+		System.out.println("Browser Version:"+browserVersion+"\n"+"Browser Name:"+browserName+"\n"+"is Visible:"+isVisible+"\n"+"Release Year:"+releaseYear+"\n"+"Browser Logo:"+browserLogo);
+		
+		
+
+	}
+
+}
