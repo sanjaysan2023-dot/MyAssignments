81322ce3b8b73a07e0535e2699fda87dcbc3d17d
sanjaysan2023-dot <sanjaysan2023@gmail.com>
2025-08-16 23:26:01 +0530

    Add files via upload
    day1_Assignment2
    Understanding Data Types and Variables in Java

---
+++ Assignment2/EmployeeDetails.java
@@ -0,0 +1,21 @@
+package week1.day1.Assignment2;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		
+		String name="John Doe";
+		int age=30;
+		double salary=55000.75;
+		char gender='M';
+		boolean acceptterms=true;
+		float rating=4.5f;
+		long mobilenumber=9876543210L;
+		int pincode=56001;
+		byte luckynumber=7;
+		
+		
+		System.out.println("Name:"+name+"\n"+"Age:"+age+"\n"+"Salary:"+salary+"\n"+"Gender:"+gender+"\n"+"Accept Terms:"+acceptterms+"\n"+"Rating:"+rating+"\n"+"Mobile Number:"+mobilenumber+"\n"+"Pincode:"+pincode+"\n"+"Lucky Number:"+luckynumber);		
+	}
+
+}
